3e8295deb551ebf90cab516e557a903484c78d6e
morishima311 <ryoryo311@gmail.com>
2019-06-24 17:29:57 +0900

    Add org inst

--- main.c
+++ main.c
@@ -25,8 +25,7 @@ typedef struct {
 } Emulator;
 
 /* Create emulator */
-static Emulator* create_emu(size_t size, uint32_t eip, uint32_t esp)
-{
+static Emulator* create_emu(size_t size, uint32_t eip, uint32_t esp){
     Emulator* emu = malloc(sizeof(Emulator));
     emu->memory = malloc(size);
 
@@ -41,15 +40,13 @@ static Emulator* create_emu(size_t size, uint32_t eip, uint32_t esp)
 }
 
 /* Destroy emulator */
-static void destroy_emu(Emulator* emu)
-{
+static void destroy_emu(Emulator* emu){
     free(emu->memory);
     free(emu);
 }
 
 /* Print registers and program counter */
-static void dump_registers(Emulator* emu)
-{
+static void dump_registers(Emulator* emu){
     int i;
 
     for (i = 0; i < REGISTERS_COUNT; i++) {
@@ -59,18 +56,15 @@ static void dump_registers(Emulator* emu)
     printf("EIP = %08x\n", emu->eip);
 }
 
-uint32_t get_code8(Emulator* emu, int index)
-{
+uint32_t get_code8(Emulator* emu, int index){
     return emu->memory[emu->eip + index];
 }
 
-int32_t get_sign_code8(Emulator* emu, int index)
-{
+int32_t get_sign_code8(Emulator* emu, int index){
     return (int8_t)emu->memory[emu->eip + index];
 }
 
-uint32_t get_code32(Emulator* emu, int index)
-{
+uint32_t get_code32(Emulator* emu, int index){
     int i;
     uint32_t ret = 0;
 
@@ -82,34 +76,41 @@ uint32_t get_code32(Emulator* emu, int index)
     return ret;
 }
 
-void mov_r32_imm32(Emulator* emu)
-{
+int32_t get_sign_code32(Emulator* emu, int index){
+    return (int32_t)get_code32(emu, index);
+}
+
+void mov_r32_imm32(Emulator* emu){
     uint8_t reg = get_code8(emu, 0) - 0xB8;
     uint32_t value = get_code32(emu, 1);
     emu->registers[reg] = value;
     emu->eip += 5;
 }
 
-void short_jump(Emulator* emu)
-{
+void short_jump(Emulator* emu){
     int8_t diff = get_sign_code8(emu, 1);
     emu->eip += (diff + 2);
 }
 
+void near_jump(Emulator* emu){
+    int32_t diff = get_sign_code32(emu, 1);
+    emu->eip += (diff + 5);
+}
+
 typedef void instruction_func_t(Emulator*);
 instruction_func_t* instructions[256];
-void init_instructions(void)
-{
+
+void init_instructions(void){
     int i;
     memset(instructions, 0, sizeof(instructions));
     for (i = 0; i < 8; i++) {
         instructions[0xB8 + i] = mov_r32_imm32;
     }
+    instructions[0xE9] = near_jump;
     instructions[0xEB] = short_jump;
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]){
     FILE* binary;
     Emulator* emu;
 
@@ -119,7 +120,7 @@ int main(int argc, char* argv[])
     }
 
     /* EIP == 0, ESP == 0x7C00 */
-    emu = create_emu(MEMORY_SIZE, 0x0000, 0x7c00);
+    emu = create_emu(MEMORY_SIZE, 0x7c00, 0x7c00);
 
     binary = fopen(argv[1], "rb");
     if (binary == NULL) {
@@ -128,7 +129,7 @@ int main(int argc, char* argv[])
     }
 
     /* Read assembly file (Max 512 byte) */
-    fread(emu->memory, 1, 0x200, binary);
+    fread(emu->memory + 0x7c00, 1, 0x200, binary);
     fclose(binary);
 
     init_instructions();
